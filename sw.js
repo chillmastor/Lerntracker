28de80ddcf835b790af3321dc54054d7c920c0ef
chillmastor <chillmastor@hotmail.de>
2026-09-05 16:48:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ const ASSETS_TO_CACHE = [
   './index.html',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/webfonts/fa-solid-900.woff2',
   'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.8.2/jspdf.plugin.autotable.min.js'
